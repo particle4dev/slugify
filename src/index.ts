fe76ed3dca4ff744f2349ae16ae0004325b0951b
Nam Hoang <>
2020-10-28 16:50:54 +0700

    saved

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+export default function aspectRatio(val: number, lim: number = 50): number[] {
+  let lower = [0, 1]
+  let upper = [1, 0]
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const mediant = [lower[0] + upper[0], lower[1] + upper[1]]
+
+    if (val * mediant[1] > mediant[0]) {
+      if (lim < mediant[1]) {
+        return upper
+      }
+      lower = mediant
+    } else if (val * mediant[1] === mediant[0]) {
+      if (lim >= mediant[1]) {
+        return mediant
+      }
+      if (lower[1] < upper[1]) {
+        return lower
+      }
+      return upper
+    } else {
+      if (lim < mediant[1]) {
+        return lower
+      }
+      upper = mediant
+    }
+  }
+}
